c6b468c915870474b8580c700b04f30735992af2
Your Namerimshaabid97@gmail.comRimshaRimsha Abiduser.email <you@example.com>
2023-05-10 15:26:03 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AppHeader from './appHeader/appHeader';
+import AppMenu from './appMenu/appMenu';
+import AppDashboard from './appDashboard/appDashboard';
+import AppFooter from './appFooter/appFooter';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <AppHeader />
+      <AppMenu />
+      <AppDashboard/>
+      <AppFooter />
+
     </div>
+    
   );
 }
 
